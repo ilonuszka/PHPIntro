0b91d3fb45b27ae9aba54389d0db701764f94103
ilonuszka <109093472+ilonuszka@users.noreply.github.com>
2023-01-12 14:52:06 +0200

    FilesUpload

---
+++ uploadFiles/index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <fieldset>
+        <legend>Uploaded files:</legend>
+        <form action="submit.php" method = 'POST' enctype="multipart/form-data">
+            <input type="file" name ="my_file"/>
+            <button type="submit">Upload</button>
+        </form>
+    </fieldset>
+    <?php
+        $jsonDecode = [];
+        $jasonGET = file_get_contents('UploadDataJson.json');
+        if($jasonGET) {
+            $jsonDecode = json_decode($jasonGET, true);
+        }
+    ?>
+
+    <fieldset>
+        <legend>Uploaded files:</legend>
+        <?php foreach ($jsonDecode as $item):?>
+        <div>
+            <div><?php echo $item['name'];?></div>
+            <div><?php echo $item['path'];?></div>
+            <div><?php echo $item['size'];?></div>
+            <div><?php echo $item['date'];?></div>
+            <form style="display: inline" method="POST" action="delete.php">
+                <input type="submit" name="delete" value="Delete" />
+                <input type="hidden" id="todoID" name="todoID" value="'.$key.'">
+            </form>
+        </div>
+        <?php endforeach;?>
+    </fieldset>
+</body>
+</html>
---
+++ uploadFiles/submit.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$error = $_FILES['my_file']['error'];
+if($error != UPLOAD_ERR_OK) {
+    echo "Error occured";
+    die;
+}
+$target_dir = "data/";
+$target_file = $target_dir .uniqid(). basename($_FILES['my_file']['name']);
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+if($imageFileType != "png" && $imageFileType != "jpeg"
+     ) {
+    echo "Sorry, only PNG, JPEG files are allowed.";
+    die;
+}
+if ($_FILES["my_file"]["size"] > 1000000) {
+    echo "Sorry, your file is too large.";
+    die;
+}
+$temporateFile = $_FILES['my_file']['tmp_name'];
+move_uploaded_file($temporateFile,$target_file);
+echo 'upload successful';
+
+$array = [];
+$array['name'] = $target_file;
+$array['size'] = $_FILES["my_file"]["size"];
+$array['path'] = $target_dir;
+$array['date'] = date_create()->format('Y-m-d H:i');
+
+$jsonDecode = [];
+$jasonGET = file_get_contents('UploadDataJson.json');
+if($jasonGET) {
+$jsonDecode = json_decode($jasonGET, true);
+}
+$jsonDecode[] = $array;
+$jasonEncode = json_encode($jsonDecode, JSON_PRETTY_PRINT);
+file_put_contents('UploadDataJson.json', $jasonEncode);
+?>
+
+</body>
+</html>
